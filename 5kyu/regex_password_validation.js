e72d97671cd44aa08fbad518f5ed7755d218e14d
sardarz <muhkween@gmail.com>
2021-01-10 10:34:02 +0300

    added description to regex password validation.js

--- 5kyu/regex_password_validation.js
+++ 5kyu/regex_password_validation.js
@@ -1,3 +1,12 @@
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+// - At least six characters long
+// - contains a lowercase letter
+// - contains an uppercase letter
+// - contains a number
+
+// Valid passwords will only be alphanumeric characters.
+
 function validate(pass) {
     return /^[A-Za-z0-9]{6,}$/.test(pass) &&
     /[A-Z]/.test(pass) &&
